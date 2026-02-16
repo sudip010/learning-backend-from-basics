df34ea83d3eb90053bb287598b4eb33fa6c388e6
Sudip <sudipbhan4@gmail.com>
2026-02-16 15:50:18 +0545

    fixed few issues in app for routes and a issue in db

--- RBAC/app.js
+++ RBAC/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/auth",require("./routes/auth.routes"));
-app.use("/api/auth",require("./routes/user.routes"));
+app.use("/api",require("./routes/user.routes"));
 
 app.get("/",(req,res)=>{
    res.send("Authentication API with jwt and RBAC"); 
